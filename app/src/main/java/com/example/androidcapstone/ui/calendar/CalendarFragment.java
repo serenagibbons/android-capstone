70c801ad58250e17b1882e7058e2b77d4702fc4a
Serena Gibbons <serenalgibbons@gmail.com>
2019-11-30 15:36:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/androidcapstone/ui/calendar/CalendarFragment.java
@@ -0,0 +1,32 @@
+package com.example.androidcapstone.ui.calendar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.androidcapstone.R;
+
+public class CalendarFragment extends Fragment {
+
+    private CalendarViewModel calendarViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        calendarViewModel =
+                ViewModelProviders.of(this).get(CalendarViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
+        /*final TextView textView = root.findViewById(R.id.text_cal);
+        notificationsViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        return root;
+    }
+}
