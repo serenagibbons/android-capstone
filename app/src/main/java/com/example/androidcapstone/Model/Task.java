6d698523ce20ad837422b324391623ffd8053e7a
Kevin Wang <Wangk6@farmingdale.edu>
2019-11-30 18:55:21 -0500

    Task Model

---
+++ app/src/main/java/com/example/androidcapstone/Model/Task.java
@@ -0,0 +1,26 @@
+package com.example.androidcapstone.Model;
+
+import java.util.Date;
+
+/*
+    Created By: Kevin Wang
+    Date: 11/30/19
+    Purpose: Used as an object of Tasks, each task has given properties. If a variable
+    has a comment, the user inputs should be consistent with the comment in order for the
+    database to match
+
+ */
+public class Task {
+    //Variables - 'F' means to follow this format
+    int m_Id; //Unique ID
+    String m_Creator; //Email of Creator
+    String [] m_AssignedTo; //Email of people assigned to, can have multiple
+    String m_TaskName; //Name of task
+    String m_TaskDescription; //Description of task
+    String m_Importance; //[F - Low, Medium, High]
+    String m_DayCompletion; //The day to be completed on [F - Mon, Tue, Wed, Thur, Fri, Sat, Sun]
+    String m_Frequency; //[F - Once, Weekly, BiWeekly]
+    Date m_CreatedOnDate; //[F - mm/dd/yy]
+    Date m_CompleteByDate; //[F - mm/dd/yy]
+    double m_Location; //Geolocation (API Possibly) that gives coordinates
+}
