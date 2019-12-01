7de6b3ae2c229d9630e58be7e3e26b9b5d3f4707
Kevin Wang <Wangk6@farmingdale.edu>
2019-11-30 23:44:43 -0500

    Create Date Picker, Priority Rater complete

--- app/src/main/java/com/example/androidcapstone/ui/create_task/CreateTaskFragment.java
+++ app/src/main/java/com/example/androidcapstone/ui/create_task/CreateTaskFragment.java
@@ -3,29 +3,79 @@ package com.example.androidcapstone.ui.create_task;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.example.androidcapstone.MainActivity;
 import com.example.androidcapstone.R;
 import com.example.androidcapstone.ui.dashboard.DashboardViewModel;
+
+import java.util.Calendar;
 
 public class CreateTaskFragment extends Fragment {
 
     private CreateTaskViewModel createTaskViewModel;
+    DatePickerDialog picker;
 
+    EditText completedBy, tempStarDisplay;
+    Button createTask;
+    RatingBar rBar;
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+                             final ViewGroup container, Bundle savedInstanceState) {
         createTaskViewModel =
                 ViewModelProviders.of(this).get(CreateTaskViewModel.class);
         View root = inflater.inflate(R.layout.fragment_create_task, container, false);
+
+        completedBy = root.findViewById(R.id.editTextCompletedByDate);
+        tempStarDisplay = root.findViewById(R.id.editTextTempStarDisplay);
+
+        //Priority Bar
+        rBar = root.findViewById(R.id.ratingBar1);
+        //Create Task Button
+        createTask = root.findViewById(R.id.buttonCreateTask);
+
+        completedBy.setInputType(InputType.TYPE_NULL);
+        completedBy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+
+                picker = new DatePickerDialog( getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                completedBy.setText(monthOfYear + "/" + (dayOfMonth + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        createTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float priority = rBar.getRating(); //Gets priority, 1.0 = Low, 2.0 = Medium, 3.0 = High
+                tempStarDisplay.setText(Float.toString(priority));
+            }
+        });
 
         return root;
     }
--- app/src/main/java/com/example/androidcapstone/ui/create_task/CreateTaskViewModel.java
+++ app/src/main/java/com/example/androidcapstone/ui/create_task/CreateTaskViewModel.java
@@ -1,5 +1,7 @@
 package com.example.androidcapstone.ui.create_task;
 
+import android.widget.EditText;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,6 +9,7 @@ import androidx.lifecycle.ViewModel;
 public class CreateTaskViewModel extends ViewModel {
     private MutableLiveData<String> mText;
 
+    EditText editText;
     public CreateTaskViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is create task fragment");
