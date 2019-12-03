c281fb0d04d41ad765799803229754338f7fc514
Serena Gibbons <serenalgibbons@gmail.com>
2019-12-03 18:34:10 -0500

    Add Contact/ContactsFragment/Contact/ViewModel/ContactAdapter 2

---
+++ app/src/main/java/com/example/androidcapstone/ui/contacts/ContactsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.androidcapstone.ui.contacts;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ContactsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ContactsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
