91de8c0db4411399034505ff5ba13a823d7d859d
Vinnyg96 <vinnyg96@homtmail.com>
2019-12-08 12:23:11 -0500

    Merge branch 'master' of https://github.com/serenagibbons/android-capstone into Vinny-Branch
    # Conflicts:
    #	app/google-services.json
    #	app/src/main/java/com/example/androidcapstone/FeedAdapter.java
    #	app/src/main/java/com/example/androidcapstone/PreLogin/SectionsPageAdapter.java
    #	app/src/main/java/com/example/androidcapstone/ui/public_feed/PublicFeedFragment.java
    #	app/src/main/res/layout/fragment_public.xml
    
    Trying to get firestore query working: OnDateSelected -> get all data from firestore with that date.

--- app/src/main/java/com/example/androidcapstone/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/androidcapstone/ui/calendar/CalendarFragment.java
@@ -1,7 +1,10 @@
 package com.example.androidcapstone.ui.calendar;
 
+import android.content.Intent;
 import android.content.ReceiverCallNotAllowedException;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +19,15 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androidcapstone.DetailedTaskActivity;
 import com.example.androidcapstone.FeedAdapter;
 import com.example.androidcapstone.Model.Task;
 import com.example.androidcapstone.R;
 import com.example.androidcapstone.ui.public_feed.PublicFeedViewModel;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -33,20 +39,23 @@ import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 
 public class CalendarFragment extends Fragment implements OnMonthChangedListener, OnDateSelectedListener{
 
     private CalendarViewModel calendarViewModel;
     private MaterialCalendarView calendarView;
     private RecyclerView calendarRecyclerView;
+    Date currentMonthSelected;
     List<CalendarDay> dates;
     List<Task> test = new ArrayList<>();
-    //private ArrayList<String> list = new ArrayList<>();
     private ArrayAdapter<String> adapter;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -81,14 +90,9 @@ public class CalendarFragment extends Fragment implements OnMonthChangedListener
 
 
                 return dates.contains(day);
-//
-//                Calendar cal1 = day.getCalendar();
-//                Calendar cal2 = Calendar.getInstance();
-//
-//                return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA)
-//                        && cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
-//                        && cal1.get(Calendar.DAY_OF_YEAR) ==
-//                        cal2.get(Calendar.DAY_OF_YEAR));
+//                FirestoreRecyclerOptions<Task> tasks = setEvents();
+//                return tasks.getSnapshots().
+
             }
 
             @Override
@@ -98,8 +102,8 @@ public class CalendarFragment extends Fragment implements OnMonthChangedListener
         });
 
 
-        displayCurrentMonthsTaskits(calendarView.getCurrentDate());
-        setUpCalRecyclerView(root);
+        //displayCurrentMonthsTaskits(calendarView.getCurrentDate());
+        //setUpCalRecyclerView(root);
 
         return root;
     }
@@ -107,6 +111,7 @@ public class CalendarFragment extends Fragment implements OnMonthChangedListener
     @Override
     public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
         displayCurrentMonthsTaskits(date);
+        currentMonthSelected = date.getDate();
     }
 
     private void displayCurrentMonthsTaskits(CalendarDay date) {
@@ -137,52 +142,103 @@ public class CalendarFragment extends Fragment implements OnMonthChangedListener
 
     }
 
-    @Override
-    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-        test.clear();
 
-        //Pull all data with m_DueDate == date.getDate();
-        //Currently pulling dummy data
-        if(dates.contains(date))
-        {
-            Task testTask = new Task();
-            //testTask.setM_CreatedOnDate(date.getDate());
-            test.add(testTask);
+    /** Returns the given date with time set to the end of the day */
+    public static Date getEnd(Date date) {
+        if (date == null) {
+            return null;
         }
-        /*// create recycler view adapter and layout manager
-        FeedAdapter adapter = new FeedAdapter(test, getActivity());
-        RecyclerView.LayoutManager manager = new LinearLayoutManager(getActivity());
-
-
-        // Link the adapter to the RecyclerView
-        calendarRecyclerView.setAdapter(adapter);
-        // Set layout for the RecyclerView
-        calendarRecyclerView.setLayoutManager(manager);
-
-        adapter.notifyDataSetChanged();*/
-
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        c.set(Calendar.MINUTE, 59);
+        c.set(Calendar.SECOND, 59);
+        c.set(Calendar.MILLISECOND, 999);
+        return c.getTime();
     }
 
-    private void setUpCalRecyclerView(View root) {
-        //Query query = publicTaskRef.orderBy("m_TaskName", Query.Direction.DESCENDING);
-        Query query = calTaskRef.orderBy("m_Task");
+    /** Returns the given date with the time values cleared. */
+    public static Date getStart(Date date) {
+        if (date == null) {
+            return null;
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        return c.getTime();
+    }
+
+    private String getDate(long time)
+    {
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(time*1000);
+        String dat = DateFormat.format("dd-MM-yyyy",cal).toString();
+        return dat;
+    }
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
 
 
+        Date d = date.getDate();
+        Query query = calTaskRef.whereEqualTo("m_DueDate", d);
+
+
+
+        Log.d("TEST",getStart(d).toString() + "\n" + d.toString());
         FirestoreRecyclerOptions<Task> tasks = new FirestoreRecyclerOptions.Builder<Task>()
                 .setQuery(query, Task.class)
                 .build();
 
-        calAdapter = new FeedAdapter(getContext(), tasks);
+
+        Log.d("TEST","SIZE: " + tasks.getSnapshots().size());
+
+        for(Task taskit : tasks.getSnapshots())
+        {
+            if(taskit.getM_DueDate() == d || taskit.getM_DueDate() != d)
+            {
+                calAdapter = new FeedAdapter(getContext(), tasks);
+
+                // refer to recycler view
+                RecyclerView recyclerView = getView().findViewById(R.id.calendar_feed_recycler_view);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                recyclerView.setAdapter(calAdapter);
+            }
+        }
 
 
-        // refer to recycler view
-        RecyclerView recyclerView = root.findViewById(R.id.calendar_feed_recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(calAdapter);
+        calAdapter.setOnItemClickListener(new FeedAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                Task task = documentSnapshot.toObject(Task.class);
+
+                String dateString = "";
+                SimpleDateFormat sdfr = new SimpleDateFormat("MM/dd/yyyy");
+                try{
+                    dateString = sdfr.format( task.getM_DueDate() );
+                }catch (Exception ex ){
+                    ex.printStackTrace();
+                }
+                String id = documentSnapshot.getId();
+                //String id = documentSnapshot.getReference().getPath();
+                Intent intent = new Intent(getContext(), DetailedTaskActivity.class);
+                intent.putExtra("taskit title", task.getM_TaskName());
+                intent.putExtra("taskit creator", task.getM_Creator());
+                intent.putExtra("taskit priority", task.getM_Importance());
+                intent.putExtra("taskit posted", task.getM_CreatedOnDate());
+                intent.putExtra("taskit deadline", dateString);
+                intent.putExtra("taskit description", task.getM_TaskDescription());
+                intent.putExtra("taskit privacy", task.getM_Privacy());
+                intent.putExtra("Document id", id);
+                startActivity(intent);
+            }
+        });
+
 
     }
-
     @Override
     public void onStart() {
         super.onStart();
